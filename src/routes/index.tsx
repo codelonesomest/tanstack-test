1aa98b4cc335af6ffd6a2107971ebeb3f469be0a
Davis <davis@Davischoos-MacBook-Pro.local>
2025-04-04 01:04:07 +0800

    Add Arcjet dependencies and implement email validation in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,43 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { createServerFn } from '@tanstack/react-start'
+
+import arcjet, { validateEmail } from "@arcjet/node";
+import { getWebRequest } from '@tanstack/react-start/server';
+
+
+const aj = arcjet({
+
+  key: "",
+
+  rules: [
+
+    validateEmail({
+
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+
+      // block disposable, invalid, and email addresses with no MX records
+
+      deny: ["DISPOSABLE", "INVALID", "NO_MX_RECORDS"],
+
+    }),
+
+  ],
+
+});
+
+
+
+const test = createServerFn({ method: "POST" }).handler(async ({ context }) => {
+  const req = getWebRequest()
+  const decision = await aj.protect(req as any, { email: "test@test.com" })
+  return decision.isAllowed()
+})
 
 export const Route = createFileRoute('/')({
   component: Home,
+  loader: async () => {
+    console.log("Is email allowed?", await test())
+  }
 })
 
 function Home() {
